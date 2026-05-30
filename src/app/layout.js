9c94fec8cdf43feae58910ae238889383e323183
ZarinTasnim75 <zarintsm15@gmail.com>
2026-05-30 21:21:49 +0600

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import dns from "node:dns";
 dns.setServers(['8.8.8.8' , '8.8.4.4']);
 
 import { Geist, Geist_Mono } from "next/font/google";
+import "animate.css";
 import "./globals.css";
 
 const geistSans = Geist({
